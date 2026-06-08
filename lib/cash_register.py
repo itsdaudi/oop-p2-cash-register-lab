977bc40fb1e385595bad7e5963e2cda721fcc4e9
daudikombe <daudikombe77@gmail.com>
2026-06-08 18:02:09 +0300

    feat:simulating a cash register for e-commerce site

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python3
-
+#creating cash register class
 class CashRegister:
+  def __init__(self, discount=0):
+    self.discount = discount
+    self.total=0
+    self.items = []
+    self.previous_transaction = []
+
   pass
